c364bba0dab7e90133f431445393ca668889ab39
HYUNJUN\sajun <sajun28@naver.com>
2023-12-01 23:03:41 +0900

    Feat: "P1874"

---
+++ src/main/java/algo/ch3/ch3_5/P1874.java
@@ -0,0 +1,39 @@
+package algo.ch3.ch3_5;
+
+import java.util.*;
+
+public class P1874 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int number = 1;
+        Stack<Integer> num = new Stack<>();
+        List<String> answer = new ArrayList<>();
+        boolean result = true;
+        for (int k : arr) {
+            if (k >= number) {
+                while (k >= number) {
+                    num.push(number++);
+                    answer.add("+");
+                }
+                num.pop();
+                answer.add("-");
+            } else {
+                if (num.pop() > k) {
+                    System.out.println("NO");
+                    result = false;
+                    break;
+                } else {
+                    answer.add("-");
+                }
+            }
+        }
+        for (String s : answer) {
+            if (result) System.out.println(s);
+        }
+    }
+}
