2c72c93b833cccc39bc77539eb9218de6776b5c0
HYUNJUN\sajun <sajun28@naver.com>
2023-12-04 15:33:19 +0900

    Feat: "P2750"

---
+++ src/main/java/algo/ch4/ch4_1/P2750.java
@@ -0,0 +1,19 @@
+package algo.ch4.ch4_1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] answer = new int[N];
+        for (int i = 0; i < N; i++) {
+            answer[i] = sc.nextInt();
+        }
+        Arrays.sort(answer);
+        for (int k : answer) {
+            System.out.println(k);
+        }
+    }
+}
