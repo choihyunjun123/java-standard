8424b8ad6d560aa3f6a5a129f7527e0302e3728c
HYUNJUN\sajun <sajun28@naver.com>
2023-12-05 19:21:30 +0900

    no message

--- src/main/java/org/example/Main3.java
+++ src/main/java/org/example/Main3.java
@@ -1,12 +1,7 @@
 package org.example;
 
-import java.util.*;
-
 public class Main3 {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int answer = sc.nextInt() - 543;
-        System.out.println(answer);
+
     }
 }
