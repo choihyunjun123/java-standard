e33eccbb1fa32dc6225f0d43f7384a7871e2349f
HYUNJUN\sajun <sajun28@naver.com>
2024-01-21 00:21:06 +0900

    Feat: "P1717"

---
+++ src/main/java/algo_back/ch8/ch8_2/P1717.java
@@ -0,0 +1,56 @@
+package algo_back.ch8.ch8_2;
+
+import java.util.*;
+
+public class P1717 {
+    static int[] parent;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            int question = sc.nextInt();
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            if (question == 0) {
+                union(a, b);
+            } else {
+                if (checkSame(a, b)) {
+                    System.out.println("yes");
+                } else {
+                    System.out.println("no");
+                }
+            }
+        }
+    }
+
+    public static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a != b) {
+            parent[b] = a;
+        }
+    }
+
+    public static int find(int a) {
+        if (a == parent[a]) {
+            return a;
+        } else {
+            return parent[a] = find(parent[a]);
+        }
+    }
+
+    private static boolean checkSame(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return true;
+        }
+        return false;
+    }
+}
